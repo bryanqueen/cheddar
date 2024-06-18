b847ca9f843a22f6e61eb42c1f6885e6c5950995
bryanqueen <somtochukwuumehdaniel@gmail.com>
2024-06-18 07:01:36 -0700

    Updated Doughnut Chart Component and bank actions

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -4,7 +4,7 @@ import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { formatAmount } from '@/lib/utils';
-import React from 'react'
+
 
 const TransactionHistory = async ({ searchParams: { id, page }}:SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -16,7 +16,7 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
       {
         label: 'Banks',
         data: balances,
-        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
+        backgroundColor: ['#FBAE26', '#000000', '#FBBE26'] 
       }
     ],
     labels: accountNames
